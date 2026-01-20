4b2762f54db3089666ed39a5b12575cd262da8dd
Mateus Chaves Moura <92216040+matemoura@users.noreply.github.com>
2026-01-20 19:16:18 -0300

    feat: add user entity and repository

---
+++ backend/src/main/java/com/mouramateus/financial/finance_api/entity/User.java
@@ -0,0 +1,27 @@
+package com.mouramateus.financial.finance_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ backend/src/main/java/com/mouramateus/financial/finance_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mouramateus.financial.finance_api.repository;
+
+import com.mouramateus.financial.finance_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
