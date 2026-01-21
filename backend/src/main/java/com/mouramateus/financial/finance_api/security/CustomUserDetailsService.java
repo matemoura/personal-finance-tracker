876b669049a921521f6b502cfadc584eea23a616
Mateus Chaves Moura <92216040+matemoura@users.noreply.github.com>
2026-01-21 01:43:10 -0300

    feat: secure api with jwt authentication filter

---
+++ backend/src/main/java/com/mouramateus/financial/finance_api/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.mouramateus.financial.finance_api.security;
+
+import com.mouramateus.financial.finance_api.entity.User;
+import com.mouramateus.financial.finance_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
+
