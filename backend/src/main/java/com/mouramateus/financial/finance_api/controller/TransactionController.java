1edcf97514b59b32436d778e757fb39802dba411
Mateus Chaves Moura <92216040+matemoura@users.noreply.github.com>
2026-01-21 18:01:10 -0300

    feat: add filters and list transactions by month

--- backend/src/main/java/com/mouramateus/financial/finance_api/controller/TransactionController.java
+++ backend/src/main/java/com/mouramateus/financial/finance_api/controller/TransactionController.java
@@ -1,12 +1,15 @@
 package com.mouramateus.financial.finance_api.controller;
 
 import com.mouramateus.financial.finance_api.dto.TransactionCreateRequest;
+import com.mouramateus.financial.finance_api.entity.CategoryType;
 import com.mouramateus.financial.finance_api.service.TransactionService;
 import com.mouramateus.financial.finance_api.entity.Transaction;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -19,5 +22,15 @@ public class TransactionController {
     @ResponseStatus(HttpStatus.CREATED)
     public Transaction create(@RequestBody @Valid TransactionCreateRequest request) {
         return transactionService.createTransaction(request);
+    }
+
+    @GetMapping
+    public List<Transaction> listByMonth(
+            @RequestParam int year,
+            @RequestParam int month,
+            @RequestParam(required = false) CategoryType type,
+            @RequestParam(required = false) Long categoryId
+            ) {
+        return transactionService.listByMonth(year, month, type, categoryId);
     }
 }
