35ba4577177d19e88088d24e768eb8c56e2337b0
Mateus Chaves Moura <92216040+matemoura@users.noreply.github.com>
2026-01-22 15:45:11 -0300

    feat: add report export in pdf and excel

---
+++ backend/src/main/java/com/mouramateus/financial/finance_api/controller/ReportController.java
@@ -0,0 +1,42 @@
+package com.mouramateus.financial.finance_api.controller;
+
+import com.mouramateus.financial.finance_api.service.ReportService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/reports")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/pdf")
+    public ResponseEntity<byte[]> exportPdf(@RequestParam int year, @RequestParam int month) {
+        byte[] pdf = reportService.generatePdf(year, month);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=reports.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdf);
+    }
+
+    @GetMapping("/excel")
+    public ResponseEntity<byte[]> exportExcel(@RequestParam int year, @RequestParam int month) {
+        byte[] excel = reportService.generateExcel(year, month);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachament; filename=report.xlsx")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(excel);
+    }
+}
